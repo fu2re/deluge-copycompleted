8d9eed6e44541eaeaa6f5d65b199cf19fca90ee9
fu2re <fu2re@yandex.ru>
2018-03-01 12:59:15 +0400

    normalize scores, doc and improvements

--- copysubtitles/copysubtitles/core.py
+++ copysubtitles/copysubtitles/core.py
@@ -52,11 +52,13 @@ from langdetect import detect_langs
 from langdetect.lang_detect_exception import LangDetectException
 import pysubs2
 
-TEST_VIDEO = re.compile('.*(' + '|'.join(['mkv', 'mp4']) + ')$')
+
+TEST_VIDEO = re.compile('.*(' + '|'.join(['mkv', 'mp4', 'avi', 'mpg']) + ')$')
 TEST_SUB1 = re.compile('.*(' + '|'.join(['ass', 'ssa']) + ')$')
 TEST_SUB2 = re.compile('.*(srt)$')
 # default density is 243 events for 23 min
 DENS = 243 / 1418930.
+ACCURACY = .75
 
 
 class TorrentCopiedEvent(DelugeEvent):
@@ -76,6 +78,10 @@ class TorrentCopiedEvent(DelugeEvent):
 
 class Core(CorePluginBase):
     def enable(self):
+        """
+
+        :return:
+        """
         self.config = deluge.configmanager.ConfigManager("copysubtitles.conf", {
             'lang': 'ru|rus'
         })
@@ -83,6 +89,10 @@ class Core(CorePluginBase):
         component.get("EventManager").register_event_handler("TorrentFinishedEvent", self.on_torrent_finished)
 
     def disable(self):
+        """
+
+        :return:
+        """
         try:
             self.timer.cancel()
         except:
@@ -94,16 +104,37 @@ class Core(CorePluginBase):
 
     @staticmethod
     def get_lang_prob(lang, text):
+        """
+        :param lang: desired language
+        :param text: contested text
+        :type lang: str
+        :type text: str
+        :return: score for the matched language
+        """
         try:
-            for l in filter(lambda p: p.lang, detect_langs(text)):
+            for l in filter(lambda p: p.lang == lang, detect_langs(text)):
                 return l.prob
             return 0
         except LangDetectException:
             return 0
 
     @staticmethod
-    def score_subtitles_folder(langs, count, location):
-        lang = langs.split('|')[0]
+    def score_subtitles_folder(languages, count, location):
+        """
+        get usability score for selected location and list of subtitle
+        file names near to their language.
+        Language is defined by simple majority vote. For example if 2 of 3
+        contested files is defined as RU - all the files will be marked as RU
+
+        :param languages: part of language regexp
+        :param count: count of subtitle files we are looking for
+        :param location: contested location
+        :type languages: str
+        :type count: int
+        :type location: str
+        :return: score (lower is better) and list of tuples. E.g. -132211, [('a.ass', 'ru'), ('b.ass', 'ru')]
+        """
+        lang = languages.split('|')[0]
         score = 0
         density = 0
         files = os.listdir(location)
@@ -114,49 +145,85 @@ class Core(CorePluginBase):
         fs = len(subs) or 10 ** -5
         f1 = len(s1)
         f2 = len(s2)
+        # number of contested lines. Lower is better for performance.
+        width = 30
 
+        # contest some files
         for filename in subs[:3]:
-            f_score = int(bool(re.search('\.(' + langs + ')+\.', filename.lower())))
+            # check existed suffix. it will be equal to 0 if it does not exist
+            f_score = int(bool(re.search('\.(' + languages + ')+\.', filename.lower())))
+            # load subtitles and check it length
             path = os.path.join(location, filename)
             sub = pysubs2.load(path)
             coverage = len(sub)
+            f_density = (coverage / float(sub[-1].end)) / DENS
 
+            # if language score is still 0 check it more closely
             if not f_score:
-                # check language for the first 100 events
-                for line in sub[:30]:
+                # we should not start from begging in case of intro
+                # that's why we try to get part from a middle
+                start = max((coverage/2) - (width/2), 0)
+                # check language for the selected part
+                for line in sub[start:(start+width)]:
                     f_score += Core.get_lang_prob(lang, line.text)
-            subs_lang.append(lang if f_score > .7 else None)
-            score += f_score / float(min(coverage, 100))
-            density += (coverage / float(sub[-1].end)) / DENS
+            # append language to majority vote list if it accurate enough
+            subs_lang.append(lang if f_score > ACCURACY else None)
+            # stack language score and density
+            score += f_score / float(min(coverage, width))
+            density += f_density
 
-        lng_score = score / fs
+        # get the final scores
+        lng_score = int((score / fs) > ACCURACY)
         cnt_score = int(fs >= count)
-        dns_score = density / fs
-        ssa_score = f1 / fs
-        srt_score = f2 / fs
+        dns_score = min(round(density / fs), 1)
+        ssa_score = round(f1 / float(fs), 2)
+        srt_score = round(f2 / float(fs), 2)
+        majority = len(set(subs_lang)) == 1
 
-        log.debug("COPYSUBTITLES: scores for %s - %s, %s, %s, %s, %s" % (location, lng_score, cnt_score, dns_score, ssa_score, srt_score))
+        log.info("COPYSUBTITLES: scores for %s - %s, %s, %s, %s, %s" % \
+                 (location, lng_score, cnt_score, dns_score, ssa_score, srt_score))
         return -(
-            lng_score * 10 ** 8 +
-            cnt_score * 10 ** 7 +
-            dns_score * 10 ** 4 +
-            ssa_score * 10 ** 3 +
+            lng_score * 10 ** 5 +
+            cnt_score * 10 ** 4 +
+            dns_score * 10 ** 3 +
+            ssa_score * 10 ** 2 +
             srt_score
-        ), zip(subs, [lang if len(set(subs_lang)) == 1 else None] * int(len(subs)))
+        ), zip(subs, [lang if majority else None] * int(len(subs)))
 
     @staticmethod
     def get_contents(location, test=None, method='walk'):
-        pathObj = FilePath(location)
-        for subpath in getattr(pathObj, method)():
-            if not test or test(subpath.path):
-                yield subpath.path
+        """
+        Get folder contents
+        :param location: contested location
+        :param test: filter function
+        :param method: one of available twisted.FilePath methods.
+        Should be on of childrens / walk
+        :return: matched paths
+        :rtype: generator
+        """
+        path_obj = FilePath(location)
+        for sub_path in getattr(path_obj, method)():
+            if not test or test(sub_path.path):
+                yield sub_path.path
 
     @staticmethod
     def get_sub_folders(location):
+        """
+        get all sub folders recursievly
+        :param location: contested location
+        :return: matched paths
+        :rtype: generator
+        """
         return Core.get_contents(location, test=lambda x: os.path.isdir(x))
 
     @staticmethod
     def get_root_folder(location):
+        """
+        Get root folder for the given path
+        :param location: contested location
+        :return: matched path
+        :rtype: str
+        """
         l2 = os.path.dirname(location)
         if not l2:
             return location
@@ -164,6 +231,13 @@ class Core(CorePluginBase):
 
     @staticmethod
     def get_video_folders(location, files):
+        """
+        Get sub folders which contains any video files
+        :param location: contested location
+        :param files: list of torrent files
+        :return: matched paths
+        :rtype: generator
+        """
         root_folders = set([Core.get_root_folder(f['path']) for f in files])
         for rf in root_folders:
             if not rf:
@@ -175,49 +249,80 @@ class Core(CorePluginBase):
                 yield d
 
     def find_subtitles(self, location):
-        files = os.listdir(location)
-        episodes_count = len(filter(TEST_VIDEO.match, files))
-        subtitle_count = len(filter(TEST_SUB1.match, files)) + len(filter(TEST_SUB2.match, files))
-        if subtitle_count >= episodes_count:
-            # subtitles already here
-            return
+        """
 
-        folders = Core.get_sub_folders(location)
-        for entry in folders:
-            score, files = Core.score_subtitles_folder(self.config["lang"], episodes_count, entry)
-            if not files:
-                continue
-            yield score, entry, files
+        :param location: contested location
+        :return:
+        :rtype: generator
+        """
+        all_files = os.listdir(location)
+        episodes_count = len(filter(TEST_VIDEO.match, all_files))
+        subtitle_files = filter(TEST_SUB1.match, all_files) + filter(TEST_SUB2.match, all_files)
+        # if subtitles already here check suffixes only
+        if len(subtitle_files) >= episodes_count:
+            yield -10 ** 10, location, subtitle_files
 
-    def find_video(self, torrent_id, video_folders, forced=False):
-        try:
-            video_folder = next(video_folders)
-            subtitle_folders = sorted(list(self.find_subtitles(video_folder)))
+        else:
 
-            if subtitle_folders:
-                _score, subtitle_folder, files = subtitle_folders[0]
-                thread.start_new_thread(
-                    Core._thread_copy, (torrent_id, video_folder, subtitle_folder, files, forced)
+            folders = Core.get_sub_folders(location)
+            for entry in folders:
+                score, files = Core.score_subtitles_folder(
+                    self.config["lang"], episodes_count, entry
                 )
-            self.find_video(torrent_id, video_folders)
-        except StopIteration:
-            return
+                if not files:
+                    continue
+                yield score, entry, files
 
     def on_torrent_finished(self, torrent_id):
         """
         Copy the torrent now. It will do this in a separate thread to avoid
         freezing up this thread (which causes freezes in the daemon and hence
         web/gtk UI.)
+        :param torrent_id:
+        :type torrent_id: int
+        :return:
         """
         torrent = component.get("TorrentManager").torrents[torrent_id]
         info = torrent.get_status(["name", "save_path", "move_on_completed", "move_on_completed_path"])
+
+        # get the destination path
         location = info["move_on_completed_path"] if info["move_on_completed"] else info["save_path"]
+
+        # we assume about any subtitle for should be forced
         _p, rest = os.path.split(location)
         forced = rest.lower() == 'anime'
-        self.find_video(torrent_id, Core.get_video_folders(location, torrent.get_files()), forced)
+
+        # lets do the job
+        video_folders = Core.get_video_folders(location, torrent.get_files())
+        for video_folder in video_folders:
+            # sort subtitle folders according to their score
+            subtitle_folders = sorted(list(self.find_subtitles(video_folder)))
+
+            if not subtitle_folders:
+                continue
+
+            _score, subtitle_folder, files = subtitle_folders[0]
+            thread.start_new_thread(
+                Core._thread_copy, (torrent_id, video_folder, subtitle_folder, files, forced)
+            )
 
     @staticmethod
     def _thread_copy(torrent_id, video_folder, subtitle_folder, files, forced):
+        """
+        copy files
+
+        :param torrent_id:
+        :param video_folder: destination folder
+        :param subtitle_folder: source folder
+        :param files: list of source files combined with language ('a.ass', 'ru')
+        :param forced: append forced suffix
+        :type torrent_id: int
+        :type video_folder: str
+        :type subtitle_folder: str
+        :type files: list
+        :type forced: boolean
+        :return:
+        """
         path_pairs = []
         for filename, lang in files:
             try:
@@ -259,12 +364,19 @@ class Core(CorePluginBase):
 
     @export()
     def set_config(self, config):
-        "sets the config dictionary"
+        """
+        sets the config dictionary
+        :param config:
+        :return:
+        """
         for key in config.keys():
             self.config[key] = config[key]
         self.config.save()
 
     @export()
     def get_config(self):
-        "returns the config dictionary"
+        """
+        returns the config dictionary
+        :return:
+        """
         return self.config.config
